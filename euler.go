f68b85bf00fa84846d91416d73d3b9ee3cb2a21e
Olivier Tharan <olivier@tharan.org>
2015-02-09 21:06:28 +0100

    Add Euler problem2 and tests

--- euler.go
+++ euler.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/olive42/euler/problem1"
+//	"github.com/olive42/euler/problem1"
+	"github.com/olive42/euler/problem2"
 )
 
 func main() {
-	fmt.Printf("Problem1: %v\n", problem1.Problem1(1000))
+//	fmt.Printf("Problem 1: %v\n", problem1.Problem1(1000))
+	fmt.Printf("Problem 2: %v\n", problem2.FibSum(4000000))
 }
