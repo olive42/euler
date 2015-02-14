9b4b9920864558c9715e4fa9339907cb2a3b1e18
Olivier Tharan <olivier@tharan.org>
2015-02-14 18:19:14 +0100

    Add comments to problem5.go Uncomment all problems in main

--- euler.go
+++ euler.go
@@ -3,15 +3,15 @@ package main
 import (
 	"fmt"
 
-	// "github.com/olive42/euler/problem1"
-	// "github.com/olive42/euler/problem2"
-	// "github.com/olive42/euler/problem4"
+	"github.com/olive42/euler/problem1"
+	"github.com/olive42/euler/problem2"
+	"github.com/olive42/euler/problem4"
 	"github.com/olive42/euler/problem5"
 )
 
 func main() {
-	// fmt.Printf("Problem 1: %v\n", problem1.Problem1(1000))
-	// fmt.Printf("Problem 2: %v\n", problem2.FibSum(4000000))
-	// fmt.Printf("Problem 4: %v\n", problem4.FindLargestPalindrome())
+	fmt.Printf("Problem 1: %v\n", problem1.Problem1(1000))
+	fmt.Printf("Problem 2: %v\n", problem2.FibSum(4000000))
+	fmt.Printf("Problem 4: %v\n", problem4.FindLargestPalindrome())
 	fmt.Printf("Problem 5: %v\n", problem5.FindSmallestMultiple(20))
 }
--- problem5/problem5.go
+++ problem5/problem5.go
@@ -1,5 +1,8 @@
+// Find the smallest integer evenly divisible by all numbers from 1 to 20.
 package problem5
 
+// IsMultipleOfEverything will return true if it is evenly divisible
+// by all integers from 1 to upto.
 func IsMultipleOfEverything(target int, upto int) bool {
 	for i := 1; i <= upto; i++ {
 		if target%i != 0 {
@@ -9,6 +12,7 @@ func IsMultipleOfEverything(target int, upto int) bool {
 	return true
 }
 
+// Factorial computes factorial(n).
 func Factorial(n int) int {
 	if n == 0 {
 		return 1
@@ -16,6 +20,8 @@ func Factorial(n int) int {
 	return n * Factorial(n-1)
 }
 
+// FindSmallestMultiple finds the smallest integer evenly divisible by
+// all numbers from 1 to upto.
 func FindSmallestMultiple(upto int) int {
 	for i := 1; i < Factorial(upto); i++ {
 		if IsMultipleOfEverything(i, upto) {
