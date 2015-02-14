68b818a34f1ce315c2be09ec0d8b70c80df22a3d
Olivier Tharan <olivier@tharan.org>
2015-02-14 11:31:21 +0100

    Fix conflict markers due to previous bad push.

--- euler.go
+++ euler.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 
-<<<<<<< HEAD
 	// "github.com/olive42/euler/problem1"
 	// "github.com/olive42/euler/problem2"
 	// "github.com/olive42/euler/problem4"
@@ -15,13 +14,4 @@ func main() {
 	// fmt.Printf("Problem 2: %v\n", problem2.FibSum(4000000))
 	// fmt.Printf("Problem 4: %v\n", problem4.FindLargestPalindrome())
 	fmt.Printf("Problem 5: %v\n", problem5.FindSmallestMultiple(20))
-=======
-//	"github.com/olive42/euler/problem1"
-	"github.com/olive42/euler/problem2"
-)
-
-func main() {
-//	fmt.Printf("Problem 1: %v\n", problem1.Problem1(1000))
-	fmt.Printf("Problem 2: %v\n", problem2.FibSum(4000000))
->>>>>>> f68b85bf00fa84846d91416d73d3b9ee3cb2a21e
 }
